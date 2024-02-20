56197389b80bc5c3ad68d2e395bcc8b8a9fe8512
Udaychandra1999 <146862819+Udaychandra1999@users.noreply.github.com>
2024-02-20 21:32:28 +0530

    Time: 114 ms (55.96%) | Memory: 17.8 MB (88.15%) - LeetSync

--- 268-missing-number/missing-number.py
+++ 268-missing-number/missing-number.py
@@ -1,10 +1,5 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        mz = max(nums)
-        mi = 0
-        res = mz+1
-        for i in range(mi,mz+1):
-            if i not in nums:
-                res = i
-        return res
+        n=len(nums)
+        return n*(n+1)//2 - sum(nums)
         
