5fc159846bd5bd4f6720f49478d9013587459b8e
Udaychandra1999 <146862819+Udaychandra1999@users.noreply.github.com>
2024-08-04 09:26:55 +0530

    Time: 50 ms (41.79%) | Memory: 16.7 MB (15.63%) - LeetSync

--- 12-integer-to-roman/integer-to-roman.py
+++ 12-integer-to-roman/integer-to-roman.py
@@ -1,44 +1,7 @@
 class Solution:
-    def intToRoman(self, num: int) -> str:
-        res =''
-        while(num>0):
-            if(num>=1000):
-                res+='M'
-                num-=1000
-            elif num>=900:
-                res+="CM"
-                num-=900
-            elif(num>=500):
-                res+='D'
-                num-=500
-            elif num>=400:
-                res+='CD'
-                num-=400
-            elif num>=100:
-                res+='C'
-                num-=100
-            elif num>=90:
-                res+='XC'
-                num-=90
-            elif num>=50:
-                res+='L'
-                num-=50
-            elif num>=40:
-                res+='XL'
-                num-=40
-            elif num>=10:
-                res+='X'
-                num-=10
-            elif num>=9:
-                res+='IX'
-                num-=9
-            elif num>=5:
-                res+='V'
-                num-=5
-            elif num>=4:
-                res+='IV'
-                num-=4
-            else:
-                res+='I'
-                num-=1
-        return res
+  def intToRoman(self, num: int) -> str:
+    M = ['', 'M', 'MM', 'MMM']
+    C = ['', 'C', 'CC', 'CCC', 'CD', 'D', 'DC', 'DCC', 'DCCC', 'CM']
+    X = ['', 'X', 'XX', 'XXX', 'XL', 'L', 'LX', 'LXX', 'LXXX', 'XC']
+    I = ['', 'I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX']
+    return M[num // 1000] + C[num % 1000 // 100] + X[num % 100 // 10] + I[num % 10]
