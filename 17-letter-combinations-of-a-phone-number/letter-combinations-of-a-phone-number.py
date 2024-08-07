b9cd75f4c15de32c4d88d2b5286f3b6e52e6bdbc
Udaychandra1999 <146862819+Udaychandra1999@users.noreply.github.com>
2024-08-07 21:15:51 +0530

    Time: 38 ms (37.13%) | Memory: 16.5 MB (35.38%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
@@ -1,29 +1,26 @@
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
-        def letter(digits:str,n:int,res,v):
+        res=[]
+        digit_map= {'2':"abc",
+                    '3':"def",
+                    '4':"ghi",
+                    '5':"jkl",
+                    '6':"mno",
+                    '7':"pqrs",
+                    '8':"tuv",
+                    '9':"wxyz"}
+
+        def letter(n:int,v):
             if len(digits) == n:
-                res.append(''.join(v))
+                res.append(v)
                 return
             for i in digit_map[digits[n]]:
-                v.append(i)
-                letter(digits,n+1,res,v)
-                v.pop()
+                letter(n+1,v+i)
+
         if len(digits)==0:
             return []
-        digit_map= {'2':['a','b','c'],
-                    '3':['d','e','f'],
-                    '4':['g','h','i'],
-                    '5':['j','k','l'],
-                    '6':['m','n','o'],
-                    '7':['p','q','r','s'],
-                    '8':['t','u','v'],
-                    '9':['w','x','y','z']}
-        if len(digits)==1:
-            return digit_map[digits]
-        else:
-            res=[]
-            v=[]
-            letter(digits,0,res,v)
+        letter(0,"")
+        
         return res
 
         
