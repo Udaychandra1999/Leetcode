ca224fbf911ce96aa50486e267eb9dc70611a67f
Udaychandra Suthari <146862819+Udaychandra1999@users.noreply.github.com>
2025-08-08 13:20:08 +0530

    Time: 6 ms (42.28%) | Memory: 17.9 MB (43.85%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.py
+++ 36-valid-sudoku/valid-sudoku.py
@@ -1,25 +1,20 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
+        rowset = defaultdict(set)
+        colset = defaultdict(set)
+        boxset = defaultdict(set)
         for i in range(9):
             for j in range(9):
                 if board[i][j] == ".":
                     continue
                 else:
-                    #horizontal check
-                    for k in range(9):
-                        if k!=j and board[i][j] == board[i][k]:
-                            return False
-                    #vertical check
-                    for k in range(9):
-                        if k!=i and board[i][j] == board[k][j]:
-                            return False
-                    #box check
-                    r,c = (i//3) * 3, (j//3) * 3
-                    for h in range(r,r+3):
-                        for v in range(c,c+3):
-                            if h != i or v != j:
-                                if board[i][j] == board[h][v]:
-                                    return False
+                    t = board[i][j]
+                    if t in rowset[i] or t in colset[j] or t in boxset[i//3,j//3]:
+                        return False
+                    rowset[i].add(t)
+                    colset[j].add(t)
+                    boxset[i//3,j//3].add(t)
         return True
+                    
                             
         
