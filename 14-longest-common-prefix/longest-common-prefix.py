867604d3bb3dbf1a49f7b34e7f19855d7ad9cb67
Udaychandra Suthari <146862819+Udaychandra1999@users.noreply.github.com>
2025-08-15 19:05:44 +0530

    Time: 4 ms (10.42%) | Memory: 18 MB (30.27%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.py
+++ 14-longest-common-prefix/longest-common-prefix.py
@@ -1,12 +1,10 @@
-from functools import reduce
-
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
-        res =""
-        for i in range(len(strs[0])):
-            for s in strs:
-                if i == len(s) or s[i]!=strs[0][i]:
+        x = strs[0]
+        res=""
+        for i in range(len(x)):
+            for s in strs[1:]:
+                if i == len(s) or x[i] !=s[i]:
                     return res
-            res+=strs[0][i]
+            res += x[i]
         return res
-        
