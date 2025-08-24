5413e61850fc21feb0bf11f43adcf31baf466788
Udaychandra Suthari <146862819+Udaychandra1999@users.noreply.github.com>
2025-08-24 19:52:39 +0530

    Time: 61 ms (5.52%) | Memory: 26 MB (28.63%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.py
+++ 238-product-of-array-except-self/product-of-array-except-self.py
@@ -1,13 +1,24 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        n = len(nums)
-        ans =[1] *n
+        lar,rar=[],[]
+        #left to right
+        i=0
+        x = 1
+        while i< len(nums):
+            if i != 0:
+                x *= nums[i-1]
+            lar.append(x)
+            i += 1
+        i = len(nums)-1
+        x = 1
+        while i>=0:
+            if i !=len(nums)-1:
+                x *= nums[i+1] 
+            rar.append(x)
+            i -= 1
+        res =[]
+        for i in range(len(nums)):
+            x = lar[i] * rar[len(nums)-i-1]
+            res.append(x)
+        return res
 
-        for i in range(1,n):
-            ans[i] = ans[i-1]*nums[i-1]
-
-        suffix =1
-        for i, num in reversed(list(enumerate(nums))):
-            ans[i]*=suffix
-            suffix*=num
-        return ans
