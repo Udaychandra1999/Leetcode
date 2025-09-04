4ddfa767184a868e8ea016057b73b6798edc2d40
Udaychandra Suthari <146862819+Udaychandra1999@users.noreply.github.com>
2025-09-04 19:26:03 +0530

    Time: 160 ms (7.49%) | Memory: 33 MB (37.94%) - LeetSync

--- 739-daily-temperatures/daily-temperatures.py
+++ 739-daily-temperatures/daily-temperatures.py
@@ -5,14 +5,8 @@ class Solution:
         res = [0]*n
         for i in range(n):
             e = (temperatures[i],i)
-            if len(stk)==0:
-                stk.append(e)
-                continue
-            if i>0 and e[0] > stk[-1][0]:
-                while len(stk)>0 and e[0]>stk[-1][0]:
-                    x = stk.pop()
-                    res[x[1]] = e[1]-x[1]
-                stk.append(e)
-            else:
-                stk.append(e)
-        return res 
+            while len(stk)>0 and e[0]>stk[-1][0]:
+                x = stk.pop()
+                res[x[1]] = e[1]-x[1]
+            stk.append(e)
+        return res  
