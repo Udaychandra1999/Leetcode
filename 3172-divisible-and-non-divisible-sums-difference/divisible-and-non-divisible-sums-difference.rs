6828ccc42ff7b6bc344bc6780b1e5f4becc62675
Udaychandra Suthari <146862819+Udaychandra1999@users.noreply.github.com>
2025-10-02 17:45:50 +0530

    Time: 0 ms (100.00%) | Memory: 2.3 MB (12.86%) - LeetSync

---
+++ 3172-divisible-and-non-divisible-sums-difference/divisible-and-non-divisible-sums-difference.rs
@@ -0,0 +1,14 @@
+impl Solution {
+    pub fn difference_of_sums(n: i32, m: i32) -> i32 {
+        let mut n1 =  0;
+        let mut n2 = 0;
+        for i in 1..n+1 {
+            if i%m==0 {
+                n2+=i;
+            }else {
+                n1+=i;
+            }
+        } 
+        n1-n2
+    }
+}
