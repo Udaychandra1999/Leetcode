b68b151fb65de811e97321caf8d2b7ea53a6a348
Udaychandra Suthari <146862819+Udaychandra1999@users.noreply.github.com>
2025-10-03 11:00:40 +0530

    Time: 0 ms (100.00%) | Memory: 2.1 MB (96.67%) - LeetSync

---
+++ 742-to-lower-case/to-lower-case.rs
@@ -0,0 +1,11 @@
+impl Solution {
+    pub fn to_lower_case(s: String) -> String {
+        s.chars().map(|c| {
+            if c>='A' && c<='Z'{
+                ((c as u8)+32) as char
+            }else {
+                c
+            }
+        }).collect()
+    }
+}
