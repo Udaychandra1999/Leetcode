93c6abf4af1d9a01e558ada121775c578b63d77d
Udaychandra Suthari <146862819+Udaychandra1999@users.noreply.github.com>
2025-10-05 09:24:59 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (48.08%) - LeetSync

--- 3194-find-words-containing-character/find-words-containing-character.c
+++ 3194-find-words-containing-character/find-words-containing-character.c
@@ -2,29 +2,14 @@
  * Note: The returned array must be malloced, assume caller calls free().
  */
 int* findWordsContaining(char** words, int wordsSize, char x, int* returnSize) {
-    int* ar;
+    int* ar = (int*)malloc(wordsSize *sizeof(int));
     int idx = 0;
-    int capacity = 4;
-    ar = (int*)malloc(capacity*sizeof(int));
-    if(ar == NULL){
-        *returnSize = 0;
-        return NULL;
-    }
     for(int i=0;i<wordsSize;i++){
         int j =0;
-        while(words[i][j]!='\0'){
+        while(words[i][j]!='\0')
+        {
             if(words[i][j]==x){
-                if(idx==capacity){
-                    capacity*=2;
-                    int* temp = (int*)realloc(ar,capacity*sizeof(int));
-                    if(temp == NULL){
-                        *returnSize = 0;
-                        return NULL;
-                    }else {
-                        ar = temp;
-                    }
-                }
-                ar[idx++] = i;
+                ar[idx++]= i;
                 break;
             }
             j++;
