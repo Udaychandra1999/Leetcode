ddf39cc28b264629063f1c07f3f661e8bf2ac23b
Udaychandra Suthari <146862819+Udaychandra1999@users.noreply.github.com>
2025-10-09 09:11:37 +0530

    Time: 0 ms (100.00%) | Memory: 28.5 MB (73.77%) - LeetSync

---
+++ 3515-find-if-digit-game-can-be-won/find-if-digit-game-can-be-won.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool canAliceWin(vector<int>& nums) {
+        int total = 0;
+        for(int x:nums){
+            total+=x;
+        }
+        int alice;
+        alice =0;
+        //single digit case
+        for(int x:nums){
+            if(x<=9){
+                alice+=x;
+            }
+        }
+        if(alice>(total-alice) || (alice<(total-alice))){
+            return true;
+        }
+        return false;
+    }
+};
