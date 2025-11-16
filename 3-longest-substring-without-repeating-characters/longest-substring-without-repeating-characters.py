897bd5e33e787a444d9c58323b44e2d1dafe5e4d
Udaychandra Suthari <146862819+Udaychandra1999@users.noreply.github.com>
2025-11-16 13:42:57 +0530

    Time: 17 ms (61.90%) | Memory: 17.8 MB (81.94%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
@@ -1,11 +1,13 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        d= dict()
-        start = 0
-        maxLen = 0
-        for i,c in enumerate(s):
-            if c in d and d[c]>=start:
-                start = d[c] +1
-            d[c] = i
-            maxLen = max(maxLen,i-start+1)
-        return maxLen
+        ch = dict()
+        l =0
+        longest = 0
+        for r in range(len(s)):
+            if s[r] in ch:
+                l = max(l,ch[s[r]]+1)
+            ch[s[r]] = r
+            longest = max(longest,(r-l+1))
+        return longest  
+                
+                
