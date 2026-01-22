53b54985e7c73920422d4235ca25831ff7a56d8b
Udaychandra Suthari <146862819+Udaychandra1999@users.noreply.github.com>
2026-01-22 12:20:24 +0530

    Time: 3 ms (83.03%) | Memory: 20.1 MB (29.34%) - LeetSync

--- 120-triangle/triangle.py
+++ 120-triangle/triangle.py
@@ -1,12 +1,9 @@
 class Solution:
     def minimumTotal(self, triangle: List[List[int]]) -> int:
         n = len(triangle)
-        dp = dict()
-        def fn(i,j):
-            if (i,j) not in dp:
-                if i == (n-1):
-                    dp[(i,j)] = triangle[i][j]
-                else:
-                    dp[(i,j)] = triangle[i][j]+min(fn(i+1,j),fn(i+1,j+1))
-            return dp[(i,j)]
-        return fn(0,0)
+        dp = triangle[-1]
+        for i in range(n-2,-1,-1):
+            for j in range(i+1):
+                dp[j] = triangle[i][j] + min(dp[j],dp[j+1])
+
+        return dp[0]
